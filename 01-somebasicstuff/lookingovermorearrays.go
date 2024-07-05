b55936ddca164cee501caf7763b1282515ba55fd
CallumMH-dev <174373194+CallumMH-dev@users.noreply.github.com>
2024-07-05 16:48:02 +0100

    Create basic arrays

--- 01-somebasicstuff/lookingovermorearrays.go
+++ 01-somebasicstuff/lookingovermorearrays.go
@@ -8,4 +8,18 @@ func main() {
 
 	fmt.Println("Looking over arrays, more specifically make, copy, append, slice!")
 
+	arrayNum := [4]int{0, 3, 2, 1}
+	fmt.Println(arrayNum)
+
+	arrayString := [5]string{
+
+		"ZERO",
+		"ONE",
+		"TWO",
+		"THREE",
+		"FOUR",
+	}
+
+	fmt.Println(arrayString)
+
 }
