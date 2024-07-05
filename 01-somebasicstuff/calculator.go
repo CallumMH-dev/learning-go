c99ac349ec909ab667db2108934005473164161a
CallumMH-dev <174373194+CallumMH-dev@users.noreply.github.com>
2024-07-05 14:10:57 +0100

    Fix str to int error
    while messing around with the syntax of Atoi
    I got an error stating "value \n" so I knew the
    number was infact being passed through Atoi and
    that i needed to trim \n from the end of the value

--- 01-somebasicstuff/calculator.go
+++ 01-somebasicstuff/calculator.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -16,20 +17,21 @@ func main() {
 
 	fmt.Println("enter first number")
 	sum1, _ := readLine.ReadString('\n')
+	trimUserData1 := strings.TrimRight(sum1, "\n")
 
 	fmt.Println("1 add, 2 sub, 3 multi, 4 divi")
-
-	operator, _ := readLine.ReadString('\n')
+	operatorCapture, _ := readLine.ReadString('\n')
+	trimOperator := strings.TrimRight(operatorCapture, "\n")
 
 	fmt.Println("enter second number")
 	sum2, _ := readLine.ReadString('\n')
+	trimUserData2 := strings.TrimRight(sum2, "\n")
 
-	println("this is sum1", sum1, "this is sum2", sum2, "this is the op", operator)
+	num1, _ := strconv.Atoi(trimUserData1)
+	num2, _ := strconv.Atoi(trimUserData2)
+	operator, _ := strconv.Atoi(trimOperator)
 
-	//calculate(&sum1, &sum2, &operator, &output)
-	num1, _ := strconv.Atoi(sum1)
-
-	fmt.Println(num1)
+	calculate(&num1, &num2, &operator, &output)
 
 	fmt.Println(output)
 }
