f20bbd5079e716843ac30166245a8c9cd147e5db
CallumMH-dev <174373194+CallumMH-dev@users.noreply.github.com>
2024-07-05 20:53:30 +0100

    test arrays, slicing, append, copy more

--- 01-somebasicstuff/lookingovermorearrays.go
+++ 01-somebasicstuff/lookingovermorearrays.go
@@ -27,4 +27,36 @@ func main() {
 
 	fmt.Println(a[0] + b[0])
 
+	dynamicArr := []int{1, 2, 3, 4, 5}
+
+	arrayCopy := make([]int, 5, 10)
+
+	fmt.Println(arrayCopy)
+
+	copy(arrayCopy, dynamicArr)
+
+	fmt.Println(dynamicArr)
+	fmt.Println(arrayCopy)
+
+	arrayCopy[2] = 99
+
+	fmt.Println(dynamicArr)
+	fmt.Println(arrayCopy)
+	slicedArr := arrayCopy[0:2]
+
+	fmt.Println(slicedArr)
+
+	appending := append(arrayCopy, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
+	//would have assumed to get an error since capacity of arrayCopy is 10 but maybe that isn't how that works
+
+	// c := [5]int{1,2,3,4,5}
+	// appendtest := append(c,1,2,3,4,5,6)
+	// ah it must be a slice.
+
+	c := make([]int, 0)
+	appendtest := append(c, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
+	fmt.Println(appending)
+	fmt.Println(len(appendtest))
+	fmt.Println(appendtest)
+
 }
